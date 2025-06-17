99754a295de9ee8369798ba082eda09e7d00fd08
Quentin Goinaud <quentin.goinaud@gmail.com>
2025-06-17 08:22:14 +0200

    -

--- keyboards/ymdk/id75/f103/keymaps/id75_azerty_colemak/keymap.c
+++ keyboards/ymdk/id75/f103/keymaps/id75_azerty_colemak/keymap.c
@@ -7,9 +7,10 @@
 // #    include "keymap.h"
 // #endif
 
-#define LAYER_LOCK_IDLE_TIMEOUT 60000  // Turn off after 60 seconds.
+#define LAYER_LOCK_IDLE_TIMEOUT 60000 // Turn off after 60 seconds.
 
 #define _AZERTY 0
+#define _AZERTY_EN 10
 #define _COLEMAK 1
 #define _FUNC 2
 
@@ -26,32 +27,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * .-----------------------------------------------------------------------------------.
     */
     [_AZERTY] = LAYOUT_ortho_5x15(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, QK_LAYER_LOCK, MO(_AZERTY),
-		TG(_COLEMAK), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_ENT, KC_HOME,
-		KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NUHS, KC_UP, KC_END,
-		KC_LCTL, KC_LGUI, KC_LOPT, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_DEL, KC_BSPC, KC_RALT, MO(_FUNC), BL_UP, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_ESC,       KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,      KC_MINS, KC_EQL,  KC_BSPC,        KC_BSPC,
+        KC_TAB,       KC_Q,    KC_W,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,      KC_LBRC, KC_RBRC, TG(_AZERTY_EN), MO(_AZERTY),
+        TG(_COLEMAK), KC_A,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,   KC_QUOT, KC_ENT,  KC_ENT,         KC_HOME,
+        KC_LSFT,      KC_NUBS, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,    KC_COMM, KC_DOT,    KC_SLSH, KC_NUHS, KC_UP,          KC_END,
+        KC_LCTL,      KC_LGUI, KC_LOPT, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_DEL, KC_BSPC, KC_RALT, MO(_FUNC), BL_UP,   KC_LEFT, KC_DOWN,        KC_RGHT
+    ),
+    [_AZERTY_EN] = LAYOUT_ortho_5x15(
+        KC_ESC,       KC_1,    KC_2,    KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,      KC_MINS, KC_EQL,  KC_BSPC,        KC_BSPC,
+        KC_TAB,       KC_A,    KC_Z,    KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,      KC_LBRC, KC_RBRC, TG(_AZERTY_EN), MO(_AZERTY),
+        TG(_COLEMAK), KC_Q,    KC_S,    KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_M,      KC_QUOT, KC_ENT,  KC_ENT,         KC_HOME,
+        KC_LSFT,      KC_NUBS, KC_W,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_COMM, KC_DOT,  KC_SLSH,   KC_SLSH, KC_NUHS, KC_UP,          KC_END,
+        KC_LCTL,      KC_LGUI, KC_LOPT, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_DEL, KC_BSPC, KC_RALT, MO(_FUNC), BL_UP,   KC_LEFT, KC_DOWN,        KC_RGHT
     ),
     [_COLEMAK] = LAYOUT_ortho_5x15(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC,
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, QK_LAYER_LOCK, MO(_AZERTY),
-		TG(_COLEMAK), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_ENT, KC_HOME,
-		KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NUHS, KC_UP, KC_END,
-		KC_LCTL, KC_LGUI, KC_LOPT, KC_SPC, KC_SPC, KC_SPC, KC_SPC, KC_DEL, KC_BSPC, KC_RALT, MO(_FUNC), BL_UP, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_A,    KC_Z,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_Q,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_W,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [_FUNC] = LAYOUT_ortho_5x15(
-        KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_PSCR,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		KC_RCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_REBOOT, QK_BOOT
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,   KC_PSCR,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,
+        KC_RCTL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_REBOOT, QK_BOOT
     )
 };
-
 
 void azerty_color(void) {
     // Blue
     rgb_matrix_sethsv_noeeprom(170, 255, 255);
+}
+
+void azerty_en_color(void) {
+    // Orange
+    rgb_matrix_sethsv_noeeprom(120, 100, 100);
 }
 
 void colemak_color(void) {
@@ -78,6 +90,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         case _AZERTY:
             azerty_color();
             break;
+        case _AZERTY_EN:
+            azerty_en_color();
+            break;
         case _COLEMAK:
             colemak_color();
             break;
@@ -90,3 +105,4 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     }
     return state;
 }
+
